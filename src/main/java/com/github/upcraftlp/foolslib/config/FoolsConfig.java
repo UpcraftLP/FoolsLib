62e3d4ade4ffc382d06691bcac64c4e67ffdbc7f
UpcraftLP <j-schirra@gmx.de>
2018-04-15 16:30:47 +0200

    config

---
+++ src/main/java/com/github/upcraftlp/foolslib/config/FoolsConfig.java
@@ -0,0 +1,44 @@
+package com.github.upcraftlp.foolslib.config;
+
+import com.github.upcraftlp.foolslib.FoolsLib;
+import com.github.upcraftlp.foolslib.api.util.EventBusSubscriber;
+import com.github.upcraftlp.foolslib.api.util.ModHelper;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class FoolsConfig {
+
+    public static Configuration config;
+    public static final String CATEGORY_GENERAL = "foolslib.config.general";
+
+    public static boolean
+            announceBetaUpdates,
+            enableUpdateChecker,
+            isDebugMode;
+
+    public static void init() {
+        config = ModHelper.getModConfig(FoolsLib.MODID, FoolsLib.VERSION);
+        config.load();
+        syncConfig();
+        config.save();
+    }
+
+    private static void syncConfig() {
+
+        enableUpdateChecker = config.getBoolean("Enable Update Checker", CATEGORY_GENERAL, true, "en/disable the update notifications");
+        announceBetaUpdates = config.getBoolean("Announce Beta Updates", CATEGORY_GENERAL, false, "enable to be informed about beta updates for mods");
+        isDebugMode = config.getBoolean("Debug Mode", CATEGORY_GENERAL, false, "show additional debug information");
+
+        if(config.hasChanged()) config.save();
+    }
+
+    @EventBusSubscriber(value = FoolsLib.MODID, fml = true)
+    public static class Handler {
+
+        @SubscribeEvent
+        public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+            if(FoolsLib.MODID.equals(event.modID)) syncConfig();
+        }
+    }
+}
