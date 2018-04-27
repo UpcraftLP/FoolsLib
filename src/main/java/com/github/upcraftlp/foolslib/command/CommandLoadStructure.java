6ca056a46e5d75ece3f635444d5b1adb9e719aa5
UpcraftLP <j-schirra@gmx.de>
2018-04-27 21:06:59 +0200

    fixed command

--- src/main/java/com/github/upcraftlp/foolslib/command/CommandLoadStructure.java
+++ src/main/java/com/github/upcraftlp/foolslib/command/CommandLoadStructure.java
@@ -2,6 +2,7 @@ package com.github.upcraftlp.foolslib.command;
 
 import com.github.upcraftlp.foolslib.FoolsLib;
 import com.github.upcraftlp.foolslib.api.world.structure.Structure;
+import com.github.upcraftlp.foolslib.api.world.structure.StructureRegistry;
 import com.github.upcraftlp.foolslib.config.FoolsConfig;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
@@ -9,6 +10,8 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
 
 public class CommandLoadStructure extends CommandBase {
     @Override
@@ -29,12 +32,21 @@ public class CommandLoadStructure extends CommandBase {
         ResourceLocation location = new ResourceLocation(args[0]);
         BlockPos pos = args.length >= 4 ? parseBlockPos(sender, args, 1, false) : sender.getPosition();
         boolean airReplaceBlocks = args.length != 5 || parseBoolean(args[4]);
-        Structure structure = Structure.REGISTRY.get(location);
+        Structure structure = StructureRegistry.getStructure(location);
         if(structure != null) {
             structure.setIntegrity(1.0F);
             structure.placeBlocksInWorld(sender.getEntityWorld(), pos, airReplaceBlocks);
         }
         else throw new CommandException("commands." + FoolsLib.MODID + "." + getCommandName() + ".structure_not_found");
+    }
+
+    @Override
+    public List addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        return
+                args.length == 1 ? getListOfStringsMatchingLastWord(args, StructureRegistry.getStructures()) :
+                args.length > 0 && args.length < 5 ? func_175771_a(args, 1, pos) :
+                args.length == 5 ? getListOfStringsMatchingLastWord(args, "true", "false") :
+                null;
     }
 
     @Override
