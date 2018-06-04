00f815152840cf9f0ead739cce1502372cb486a0
UpcraftLP <j-schirra@gmx.de>
2018-06-04 13:53:01 +0200

    fixed spawn event

--- src/main/java/com/github/upcraftlp/foolslib/api/luck/event/EventSpawn.java
+++ src/main/java/com/github/upcraftlp/foolslib/api/luck/event/EventSpawn.java
@@ -10,6 +10,8 @@ import net.minecraft.world.World;
 
 public class EventSpawn extends LuckyEvent {
 
+    //FIXME living entities do not spawn
+
     /**
      * maximum spawn attempts per entity that is trying to spawn
      */
@@ -32,32 +34,36 @@ public class EventSpawn extends LuckyEvent {
 
     @Override
     public void activate(ResourceLocation block, World world, BlockPos pos, EntityPlayerMP player, int luck) {
-        spawnLoop:
+        if(types.length == 0) return;
+
         for(int i = 0; i < minCount + RANDOM.nextInt(maxCount - minCount + 1); i++) {
             Class<? extends Entity> clazz = this.types[RANDOM.nextInt(types.length)];
             try {
-                Entity entity = clazz.getConstructor(new Class[] {World.class}).newInstance(world);
-                //see http://mathworld.wolfram.com/DiskPointPicking.html
-                for(int j = 0; j < MAX_SPAWN_ATTEMPTS; j++) {
-                    double angle = Math.toRadians(360 * RANDOM.nextDouble());
-                    double sqrt_r = Math.sqrt(RANDOM.nextDouble() * maxRadius);
-                    double xOffset = sqrt_r * Math.cos(angle);
-                    double zOffset = sqrt_r * Math.sin(angle);
-                    double x = pos.getX() + 0.5D + xOffset;
-                    double y = pos.getY() + 0.02D;
-                    double z = pos.getZ() + 0.5D + zOffset;
-                    BlockPos spawnPos = new BlockPos(x, y, z);
-                    if(world.isBlockLoaded(spawnPos) && world.isAirBlock(spawnPos) && world.isAirBlock(spawnPos.up())) {
-                        entity.setLocationAndAngles(x, y, z, RANDOM.nextFloat(), 0.0F);
-                        if(entity instanceof EntityLiving) {
-                            EntityLiving entityLiving = (EntityLiving) entity;
-                            entityLiving.setAttackTarget(player);
-                            entityLiving.onInitialSpawn(world.getDifficultyForLocation(spawnPos), null);
+                spawnLoop:
+                for(int count = RANDOM.nextInt(4); count > 0; count--) {
+                    Entity entity = clazz.getConstructor(new Class[] {World.class}).newInstance(world);
+                    for(int j = 0; j < MAX_SPAWN_ATTEMPTS; j++) {
+                        //see http://mathworld.wolfram.com/DiskPointPicking.html
+                        double angle = Math.toRadians(360 * RANDOM.nextDouble());
+                        double sqrt_r = Math.sqrt(RANDOM.nextDouble() * maxRadius);
+                        double xOffset = sqrt_r * Math.cos(angle);
+                        double zOffset = sqrt_r * Math.sin(angle);
+                        double x = pos.getX() + 0.5D + xOffset;
+                        double y = pos.getY() + 0.02D;
+                        double z = pos.getZ() + 0.5D + zOffset;
+                        BlockPos spawnPos = new BlockPos(x, y, z);
+                        if(world.isBlockLoaded(spawnPos) && world.isAirBlock(spawnPos) && world.isAirBlock(spawnPos.up())) {
+                            entity.setLocationAndAngles(x, y, z, RANDOM.nextFloat(), 0.0F);
+                            if(entity instanceof EntityLiving) {
+                                EntityLiving entityLiving = (EntityLiving) entity;
+                                entityLiving.onInitialSpawn(world.getDifficultyForLocation(spawnPos), null);
+                                entityLiving.setAttackTarget(player);
+                            }
+                            world.spawnEntityInWorld(entity);
+                            continue spawnLoop;
                         }
-                        world.spawnEntityInWorld(entity);
-                        continue spawnLoop;
+                        entity.setDead();
                     }
-                    entity.setDead();
                 }
             } catch(Exception e) {
                 FoolsLib.getLogger().error("unable to spawn entity from class " + clazz.getCanonicalName(), e);
